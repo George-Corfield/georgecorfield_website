12e8048d5770de06488270f506df6f13c9810cdb
georgecorfield <gacorfield50@gmail.com>
2024-09-26 00:58:04 +0100

    projects page finished

--- src/Components/Contact/Contact.tsx
+++ src/Components/Contact/Contact.tsx
@@ -1,6 +1,20 @@
 function Contact() {
     return (
-        <div>Contact</div>
+        <div className="contact page">
+            <div className="headlines">
+                <h2>Contact</h2>
+            </div>
+            <div className="content">
+                <span className="text">
+                    Feel free to reach out to me via the email form below. Alternatively, connect and message me on LinkedIn :)
+                </span>
+                <div className="contact-form">
+                    <form>
+                        
+                    </form>
+                </div>
+            </div>
+        </div>
     )
 }
 
